bd94847a9ba682a2ab22994b37c7a7b40b31f1cc
Ilya Taskaev <ansustiwaz@gmail.com>
2021-10-04 15:52:13 +0300

    fix

--- terraform/fargate_app/modules/ecs/outputs.tf
+++ terraform/fargate_app/modules/ecs/outputs.tf
@@ -2,5 +2,5 @@ output "log_group_name" {
   value = aws_cloudwatch_log_group.log_group.name
 }
 output "lb_dns_name" {
-  value = aws_lb.public_lb.dns_name
+  value = aws_lb.public_lb[0].dns_name
 }
